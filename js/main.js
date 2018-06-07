d5fea9eeed2eecf05a9425a2a11428c5cd6bc659
tdnvl <bummel1889@gmail.com>
2018-06-06 22:48:34 -0400

    Move script to main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+            Particles.init({
+                selector: ".background",
+                maxParticles: 110,
+                color: "#75A5B7",
+                connectParticles: true
+            });
+        };
